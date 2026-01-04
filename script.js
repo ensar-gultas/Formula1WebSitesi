d8fbcfff7a8a0bc2da4947ce595eb2a87a98de5f
Ensar Gültaş <ensar24gultas@gmail.com>
2026-01-04 12:36:48 +0300

    Proje güncellendi

---
+++ script.js
@@ -0,0 +1,14 @@
+function takimSec(){
+	let takim = document.getElementById("favori-takim").value;
+	console.log(takim);
+}
+
+function pilotSec(){
+	let pilot = document.getElementById("favori-pilot").value;
+	console.log(pilot);
+}
+
+function pistSec(){
+	let pist = document.getElementById("favori-pist").value;
+	console.log(pist);
+}
